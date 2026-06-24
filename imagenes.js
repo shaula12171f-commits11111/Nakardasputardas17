06bb27b4c38fab6aeea6035011cad612c83f0965
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-06-24 18:03:25 -0500

    Update imagenes.js

--- imagenes.js
+++ imagenes.js
@@ -249,12 +249,34 @@ lamiendo_pene:  { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDD
         descripcion: "Semielfa de Hielo - Amable, pura, determinada y noble. Cabello plateado y ojos violeta.",
         imagenSelector: "https://i.pinimg.com/736x/4e/9a/9c/4e9a9c8f6b4e3d2a1f5c8b7e9d0a2c4f.jpg",
         imagenes: {
-            hablando: { url: "https://i.pinimg.com/736x/4e/9a/9c/4e9a9c8f6b4e3d2a1f5c8b7e9d0a2c4f.jpg", audio: "" },
-            besando: { url: "https://i.pinimg.com/736x/8a/2b/3c/8a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d.jpg", audio: "" },
-            desnuda: { url: "https://i.pinimg.com/736x/1a/2b/3c/1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d.jpg", audio: "" },
-            ropa_elegante: { url: "https://i.pinimg.com/736x/2a/3b/4c/2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d.jpg", audio: "" },
-            abrazo: { url: "https://i.pinimg.com/736x/3a/4b/5c/3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d.jpg", audio: "" },
-            sonriendo: { url: "https://i.pinimg.com/736x/4a/5b/6c/4a5b6c7d8e9f0a1b2c3d4e5f6a7b8c9d.jpg", audio: "" },
+   // Emilia de Re:Zero - Cabello plateado, medias blancas, vestido blanco con morado
+        hablando: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773017610297.jpg", audio: "" },
+           Caminando_al_cuarto: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD5/main/imagenes/img_1773192736226.jpg", audio: "" },
+           besando: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773017616081.jpg", audio: "" },
+         chupando_solo_la_cabeza_del_pene: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773017717220.jpg", audio: "" },
+        chupando_hasta_la_mitad_del_pene: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773017829239.jpg", audio: "" },
+    chupando_todo_el_pene: { url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773067940965.jpg", audio: "https://files.catbox.moe/cmyr3p.mp3" },
+      
+        chupando_todo_el_pene_mano_en_su_cabeza_empujandola: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773018592671.jpg", audio: "https://files.catbox.moe/dwvfoi.mp3" },
+      chupando_bolas: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD5/main/imagenes/img_1773192778522.jpg", audio: "https://files.catbox.moe/fz1omb.mp3" },
+       chupando_bola_izquierda: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD5/main/imagenes/img_1773192798552.jpg", audio: "" },
+    chupando_bola_derecha: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD5/main/imagenes/img_1773192796496.jpg", audio: "" },
+   
+       
+        doggystyle: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773018797868.jpg", audio: "https://files.catbox.moe/cpu9th.mp3" },
+      sidefuck: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773018651809.jpg", audio: "" },
+      
+        follando_en_el_aire: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772926402004.jpg", audio: "https://files.catbox.moe/iakcoo.mp3" }, 
+           cowgirl: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773018200154.jpg", audio: "" },
+         reverse_cowgirl: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773018223555.jpg", audio: "" },
+        misionero: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773018628504.jpg", audio: "" },
+       licking_pussy: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773018428245.jpg", audio: "" },
+          fingering_pussy: { url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773018439864.jpg", audio: "" },
+        
+        
+        mostrando_culo_sin_ropa: { url: "https://pbs.twimg.com/media/HIhW093aIAAD1-i?format=jpg&name=large", audio: "" },
+           mostrando_culo_y_tanga_morada: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD7/main/imagenes/img_1773342726489.jpg", audio: "" },
+        pene_rozando_su_ano: { url: "https://files.catbox.moe/kaftvc.png", audio: "" },
         }
     }
 };
