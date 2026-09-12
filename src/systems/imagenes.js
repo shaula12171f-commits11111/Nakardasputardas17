c71a155545563ad34cf917651e78e11acde34956
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-12 10:16:20 -0500

    add/update Miku.bikini_playa (ROPA EXTRA (Nino))

--- src/systems/imagenes.js
+++ src/systems/imagenes.js
@@ -613,7 +613,9 @@ const QuintiImagenesPrueba = {
             // 74. moviendo_el_culo
             // falta
 
-            // === 75-77  EXTRAS NINO (agarres) ===
+                        bikini_playa: { url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Miku/bikini_playa.jpg", audio: "", descripcion: "sombreo de paja grande liston azul bikini dorado" },
+
+// === 75-77  EXTRAS NINO (agarres) ===
             // 75. usuario_agarra_el_culo_a_nino_NOSEX
             // falta
             // 76. usuario_agarra_el_culo_a_nino_
