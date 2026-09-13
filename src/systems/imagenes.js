d42cf6f358e581fc48ad65a64c3ee1684393420a
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-13 12:33:36 -0500

    add/update Nino.assjob_cumming (HANDJOB / PAJA / ASSJOB)

--- src/systems/imagenes.js
+++ src/systems/imagenes.js
@@ -229,7 +229,9 @@ const QuintiImagenesPrueba = {
             assjob: { url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Nino/assjob.jpg", audio: "", descripcion: "assjob mi verga entre sus nalgas" },
             handjob_paja: { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD58/main/imagenes/img_1780228236266.jpg", audio: "https://litter.catbox.moe/gybisrc3ys50tlum.mp3", descripcion: "" },
 
-            // =====================================================
+                        assjob_cumming: { url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1784504256061.png", audio: "", descripcion: "me corro mientras nino me hace una assjob" },
+
+// =====================================================
             //  DOGGYSTYLE
             // =====================================================
             doggystyle: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD8/main/imagenes/img_1773407766732.jpg", audio: "https://files.catbox.moe/04emjp.mp3", descripcion: "" },
