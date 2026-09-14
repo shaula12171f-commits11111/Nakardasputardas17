ae711f17ef0b977d770e0b3c549445c6be1e57e8
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-14 14:59:10 -0500

    add/update Nino.nino_bebiendo_semen_del_condon_lleno (CONDON)

--- src/systems/imagenes.js
+++ src/systems/imagenes.js
@@ -207,6 +207,8 @@ const QuintiImagenesPrueba = {
             nino_quiere_usar_condones_al_follar: { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD74/main/imagenes/img_1788102164009.jpg", audio: "", descripcion: "nino quiere usar condones" },
             nino_comprando_condones: { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD74/main/imagenes/img_1788102172779.jpg", audio: "", descripcion: "nino compra condones" },
 
+            nino_bebiendo_semen_del_condon_lleno: { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD74/main/imagenes/img_1788105124286.jpg", audio: "", descripcion: "nino setraga el semen del condon despues de follar" },
+
 // =====================================================
             //  BESOS
             // =====================================================
