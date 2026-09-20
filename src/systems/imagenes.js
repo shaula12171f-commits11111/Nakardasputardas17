74c6f217d572965c0cb253a2cf16ae1374dc3893
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-20 14:07:48 -0500

    Update imagenes.js

--- src/systems/imagenes.js
+++ src/systems/imagenes.js
@@ -501,13 +501,14 @@ const QuintiImagenesPrueba = {
             meto_dedos_en_su_concha_nino_se_corre: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD22/main/imagenes/img_1773854105419.jpg", audio: "", descripcion: "nino se corre cuando le meto los deddos en su coño concha" },
 
             nino_squirt: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD22/main/imagenes/img_1773854107557.jpg", audio: "", descripcion: "nino squirtea despues de jugar con su coño con mis dedos" },
-}
-
-    matting_press: {
+     nino_matting_press: {
       url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Nino/matting_press.jpg",
       audio: "",
       descripcion: "la follo matting press ella trata de contener sus gemidos"
-    },
+    },       
+}
+ 
+   
 },
 
     Miku: {
