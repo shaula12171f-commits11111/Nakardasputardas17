2ab9a2325303a9e00fedb7cd96c0e3ab7e9161cd
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-22 17:01:37 -0500

    add/update Nino.ropa_solo_una_toalla (editor unificado)

--- src/systems/imagenes.js
+++ src/systems/imagenes.js
@@ -412,7 +412,7 @@ const QuintiImagenesPrueba = {
             ropa_sacerdotisa: { url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Nino/ropa_sacerdotisa.jpg", audio: "", descripcion: "ropa de sacerdotisa tacones yukata blanco y rojo liston rojo" },
 
             nino_pijama: { url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Nino/nino_pijama.jpg", audio: "", descripcion: "nino pijama transparente blanco moñito en el pijama moño negro" },
-
+            ropa_solo_una_toalla: { url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Nino/ropa_solo_una_toalla.jpg", audio: "", descripcion: "solo lleva puesta una toalla" },
 // =====================================================
             //  COSPLAY
             // =====================================================
