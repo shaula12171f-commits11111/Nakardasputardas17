4b4bebf5c60459061cde32bad18d324d0e30c61b
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-26 13:02:15 -0500

    add/update Miku.pongo_mi_verga_en_la_Raja_que_separa_su_culo (editor unificado)

--- src/systems/imagenes.js
+++ src/systems/imagenes.js
@@ -530,6 +530,7 @@ const QuintiImagenesPrueba = {
             miku_bañandose: { url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Miku/miku_bañandose.jpg", audio: "", descripcion: "miku se baña" },
             Miku_matting_press: { url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Miku/Miku_matting_press.jpg", audio: "", descripcion: "follando en matting press a miku ella trata de contener sus gemidos" },
             la_follo_mientras_la_ahorco: { url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Miku/la_follo_mientras_la_ahorco.jpg", audio: "", descripcion: "la follo mientras la ahorco mi brazo alrededor de su cuello" },
+            pongo_mi_verga_en_la_Raja_que_separa_su_culo: { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD81/main/imagenes/img_1789928426502.png", audio: "", descripcion: "pongo mi verga en medio de sus nalgas sin meterla" },
 // =====================================================
             //  BESOS
             // =====================================================
