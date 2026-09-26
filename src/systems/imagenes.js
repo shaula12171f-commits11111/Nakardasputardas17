ab31a655b1cce5f4382fc4f54235c4417cc3d673
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-26 13:05:05 -0500

    add/update Miku.miku_desnuda_en_la_cama_de_costado (editor unificado)

--- src/systems/imagenes.js
+++ src/systems/imagenes.js
@@ -531,6 +531,7 @@ const QuintiImagenesPrueba = {
             Miku_matting_press: { url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Miku/Miku_matting_press.jpg", audio: "", descripcion: "follando en matting press a miku ella trata de contener sus gemidos" },
             la_follo_mientras_la_ahorco: { url: "https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/Miku/la_follo_mientras_la_ahorco.jpg", audio: "", descripcion: "la follo mientras la ahorco mi brazo alrededor de su cuello" },
             pongo_mi_verga_en_la_Raja_que_separa_su_culo: { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD81/main/imagenes/img_1789928426502.png", audio: "", descripcion: "pongo mi verga en medio de sus nalgas sin meterla" },
+            miku_desnuda_en_la_cama_de_costado: { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD81/main/imagenes/img_1789928521502.png", audio: "", descripcion: "miku esta acostada desnuda en mi cama de costado" },
 // =====================================================
             //  BESOS
             // =====================================================
